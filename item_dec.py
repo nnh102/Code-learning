6179ff6f87f14619194707d077ea1ae0d954f390
nnh102 <ha.nguyen@dena.jp>
2014-03-26 03:50:58 +0000

    add item_dec.py

---
+++ item_dec.py
@@ -0,0 +1,24 @@
+
+import re
+patt = re.compile("[^\t\r\n]+")
+print "start"
+n =1
+for x in range(18, 31):
+	print "process", "blood.present_receive.log.201312%01d" % x
+	f = open("blood.present_receive.log.201312%01d" % x)
+	item_dec = {}
+	for line in f:
+		#n += 1
+		#if n > 100:
+		#	break
+		a = patt.findall(line)
+		for i in xrange(4, 8):
+			a[i] = a[i][1:-1].split(",")
+		for m in xrange(0, len(a[4])):
+			if a[5][m] == "1" and a[4][m] == "1":
+				if a[6][m] not in item_dec:
+					item_dec[a[6][m]] = int(a[7][m])
+				else:
+					item_dec[a[6][m]] += int(a[7][m])
+	print item_dec
+
